19e064a75f4827bc5336512e4ddc4a37a3d069e8
Justin Sorensen <justinsorensen83@gmail.com>
2013-03-18 21:25:28 -0700

    dd

--- main.js
+++ main.js
@@ -1,56 +1,55 @@
-var http = require('http'),
-    assert = require('assert'),
-    fs = require('fs'),
-    url = require('url'),
-    path = require('path');
+var http    = require('http');
+var fs      = require('fs');
 
-var port = 5000;
-var webPath = 'public';
-var mimeTypes = {
-  '.js' : 'application/javascript',
-  '.html' : 'text/html',
-  '.jpeg' : 'image/jpeg',
-  '.jpg' : 'image/jpg',
-  '.gif' : 'image/gif',
-  '.css' : 'text/css',
-  '.ico' : 'image/x-icon'
-};
+var server = http.createServer(function (request, response) {
 
-function requestHandler(req, res) {
-  var requestedUrl = url.parse(req.url).pathname;
-  if(requestedUrl === '/') requestedUrl += 'index.html';
-  var requestedFile = path.join(webPath, requestedUrl);
-  console.log(req.connection.remoteAddress + ' requested file ' + requestedFile);
-  fs.exists(requestedFile, function(exists) {
-    if(!exists) {
-      res.writeHead(404, {
-        'Content-Type': 'text/plain'
-      });
-      res.write('404 Not Found');
-      res.end();
-      return;
-    }
-    fs.readFile(requestedFile, 'binary', function(err, file) {
-      if(err) {
-        res.writeHead(500, {
-          'Content-Type': 'text/plain'
+		//Serving the static files
+    if (request.method === "GET"){
+	
+			//Files will be stored in "public" folder
+			//If ternary doesn't work, switch back to "var = if/else" statement
+        var url = './public' + (request.url == '/' ? '/index.html' : request.url); 
+				
+			//Using included "file system" functionality
+			//Other plugins also available to handle these kinds of requests
+		
+			//fs.readFileSync(filename,[encoding]) synchronous alternative
+			//fs.readFile(filename,[encoding],[callback]) asynchronous	
+        fs.readFile(url, function(error, content) {
+            if(error) {
+                console.log('error loading file ' + url + ': ', error);
+                response.writeHead(404);
+                response.end();
+            }else {
+					// Check mime type
+                var temp = url.lastIndexOf(".");	//string.lastIndexOf(searchValue[, fromIndex])
+                var ext = url.substring((temp));
+                var mime = mimes[ext] || 'text/plain';
+				
+					// Write the file
+					//response.writeHead(statusCode,[reasonPhrase],[headers])
+                response.writeHead(200, { 'Content-Type': mime });
+				
+					//response.end([data],[encoding])
+                response.end(content, 'utf-8');
+            }
         });
-        res.write(err);
-        res.end();
-        return;
-      }
-      var type = mimeTypes[path.extname(requestedFile)];
-      res.writeHead(200, {
-        'Content-Type': type
-      });
-      res.write(file, 'binary');
-      res.end();
-    });
-  });
-}
-
-
-var server = http.createServer(requestHandler);
-server.listen(port, function() { 
-  console.log('Listening to the port 5000');  
+    }
 });
+ 
+	// Listen to port 5000, log to console
+server.listen(5000, function() {
+  console.log('Listening to port ' + server.address().port);
+  });
+ 
+	//Mime type definitions
+var mimes = {
+    '.css' :  'text/css',
+    '.js' :   'text/javascript',
+    '.htm' :  'text/html',
+    '.html' : 'text/html',
+	'.png' : 'image/png',
+	'.gif' : 'image/gif',
+	'.jpg' : 'image/jpeg',
+	'.bmp' : 'image/bmp'
+};
